4fbdaa8f27241eb0ae48a6c40130353a32598fe5
Aiswarya0302 <ishuu0317@gmail.com>
2025-11-11 19:19:03 +0530

    Add files via upload

---
+++ assin.sql
@@ -0,0 +1,13 @@
+create database SchoolDB;
+use SchoolDB;
+show databases;
+create table Students(
+student_ID int primary key ,
+student_name varchar(50),
+student_age varchar(40),
+grade varchar(30)
+);
+
+
+desc Students;
+
